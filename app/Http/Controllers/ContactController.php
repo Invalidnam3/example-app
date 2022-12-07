f97a20e08166629202edd64e17263bc195c6a07d
Andrés A <andres.acostahe@gmail.com>
2022-12-07 08:45:28 -0500

    Reached lesson 41

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,34 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Repositories\CompanyRepository;
 
 class ContactController extends Controller
 {
-    public function index() {
-      $companies = [
-        1 => ['name' => 'Company One', 'contacts' => 3],
-        2 => ['name' => 'Company Two', 'contacts' => 5],
-      ];
-      $contacts = $this->getContacts();
-      return view('contacts.index', compact('contacts', 'companies'));
-    }
+  public function __construct(protected CompanyRepository $company)
+  {
 
-    public function create() {
-      return view('contacts.create');
-    }
+  }
 
-    public function show($id) {
-      $contacts = $this->getContacts();
-      abort_if(!isset($contacts[$id]), 404);
-      $contact = $contacts[$id];
-      return view('contacts.show')->with('contact', $contact);
-    }
+  public function index(CompanyRepository $company, Request $request)
+  {
+    $companies = $this->company->pluck();
+    $contacts = $this->getContacts();
+    return view('contacts.index', compact('contacts', 'companies'));
+  }
 
-    private function getContacts () {
-      return [
-        1 => ['id' => 1, 'name' => 'Name 1', 'phone' => '1234567890'],
-        2 => ['id' => 2, 'name' => 'Name 2', 'phone' => '2345678901'],
-        3 => ['id' => 3, 'name' => 'Name 3', 'phone' => '3456789012'],
-      ];
-    }
+  public function create()
+  {
+    return view('contacts.create');
+  }
+
+  public function show($id)
+  {
+    $contacts = $this->getContacts();
+    abort_if(!isset($contacts[$id]), 404);
+    $contact = $contacts[$id];
+    return view('contacts.show')->with('contact', $contact);
+  }
+
+  private function getContacts()
+  {
+    return [
+      1 => ['id' => 1, 'name' => 'Name 1', 'phone' => '1234567890'],
+      2 => ['id' => 2, 'name' => 'Name 2', 'phone' => '2345678901'],
+      3 => ['id' => 3, 'name' => 'Name 3', 'phone' => '3456789012'],
+    ];
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\ActivityController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 // Controllers
@@ -20,8 +24,14 @@ use App\Http\Controllers\WelcomeController;
 
 Route::get('/', WelcomeController::class);
 
-Route::controller(ContactController::class)->name('contacts.')->group(function() {
-  Route::get('/contacts', 'index')->name('index');
-  Route::get('/contacts/create', 'create')->name('create');
-  Route::get('/contacts/{id}', 'show')->name('show');
-});
+Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
+Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
+Route::get('/contacts/{id}', [ContactController::class, 'show'])->name('contacts.show');
+Route::resource('/company', CompanyController::class);
+Route::resources([
+  '/tags' => TagController::class,
+  '/tasks' => TaskController::class
+]);
+Route::resource('/activities', ActivityController::class)->except([
+  'index', 'show'
+]);
